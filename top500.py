fabc11cac9d4b4a5b9ff68c5f3e8d3b09f847cb3
ZeroMarker <u78k4l67@163.com>
2023-04-30 15:09:17 +0000

    Import to replit

---
+++ top500.py
@@ -0,0 +1,9 @@
+import pandas as pd
+import lxml
+
+
+url = "https://www.fortunechina.com/fortune500/c/2022-08/03/content_415683.htm"
+data = pd.read_html(url)[0]
+data.drop("关键数据", axis=1, inplace=True)
+# data.to_excel("D:\\top500.xlsx", index=False)
+print(data["国家"].value_counts())
